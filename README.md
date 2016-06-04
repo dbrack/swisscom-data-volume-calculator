3b58200fb530b6666baa98a99f27b1bf6cfa54d3
Dominik Brack <dominik.brack@gmail.com>
2016-06-04 19:56:27 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Swisscom data volume calculator
+
+Parses a CSV file from Swisscom's account center.
+It outputs the total data volume that was used within the time period.
+
+Because, who want's to use Excel!
+
+Make sure to remove the header (the veryi first line) from the CSV file before running the script.
+
+To use this script, run
+
+`babel-node volume.js ~/path/to/exported/file.csv`
---
+++ volume.js
@@ -0,0 +1,17 @@
+import fs from 'fs'
+
+if (process.argv.length !== 3) {
+	console.log('Usage: babel-node volume.js [csvFilePath]');
+	process.exit(1);
+}
+
+const fileName = process.argv[2]
+const volume = fs.readFileSync(fileName, 'utf8')
+  .trim()
+  .split('\n')
+  .map(line => line.split(';'))
+  .filter(line => line[4].toLowerCase().indexOf('volumentaxiert') !== -1)
+  .map(line => parseInt(line[5], 10))
+  .reduce((p, c) => p + c) / 1000000
+
+console.log('Total GB used:', volume)
